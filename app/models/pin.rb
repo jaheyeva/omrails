b9bb807ffeed13902777452d880ca1003794c22d
jaheyeva <jaheyeva@gmail.com>
2013-09-12 16:25:30 -0500

    Add description validation

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
   attr_accessible :description
-
-  validates :description, presence: true, length: { minimum: 1 }
+  validates :description, presence: true, length: { in: 3..10, too_short: "Description is too short. Minimum %{count} characters are required.",
+    too_long: "%{count} characters is the only maximum allowed" }
 end

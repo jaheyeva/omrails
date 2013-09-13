e90d37ced5506602fbee8d03f81d687ff48f1060
jaheyeva <jaheyeva@gmail.com>
2013-09-13 12:13:21 -0500

    Add Pins Associations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,9 @@
 class Pin < ActiveRecord::Base
   attr_accessible :description
-  
+
   validates :description, presence: { :message => "Please add description." }, length: { in: 3..20, too_short: "Description is too short. Minimum %{count} characters are required.",
     too_long: "%{count} characters is the only maximum allowed" }
+
+   	belongs_to :user
+   	validates :user_id, presence: true
 end
